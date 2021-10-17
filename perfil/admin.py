ca8790baf0f276081ad8d6765ced77d6fd9ccfa9
Benites Enzo <enzostefani507@gmail.com>
2021-10-17 18:11:35 -0300

    estado | admins podran dar sombreros grises de forma masiva

--- perfil/admin.py
+++ perfil/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Usuario)
+
+
+class habilitarCambioAdmin(admin.ModelAdmin):
+    actions = ['habilitarCambio']
+
+    def habilitarCambio(self,request,queryset):
+        queryset = queryset.exclude(gris_disponible = True)
+        for miembro in queryset:
+            miembro.gris_disponible = True
+            miembro.save()
+
+admin.site.register(Usuario,habilitarCambioAdmin)
 admin.site.register(Equipo)
