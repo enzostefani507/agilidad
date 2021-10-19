c69c46973d1c0573edf1aa270160060cc7865261
Benites Enzo <enzostefani507@gmail.com>
2021-10-18 22:22:53 -0300

    estado & home | Podia de dorados

--- home/views.py
+++ home/views.py
@@ -1,14 +1,16 @@
 from django.shortcuts import redirect, render
 from django.http import HttpResponse
 from .models import *
-
+from perfil.models import Usuario
 def home(request):
     temas = Tema.objects.all()
     unidades = Unidad.objects.all()
+    ranking = Usuario.objects.filter(dorado__gt=0).order_by('dorado').reverse()
     context ={
         'nombreSeccion':'Saludos!',
         'contenido': temas,
-        'unidades':unidades
+        'unidades':unidades,
+        'ranking': ranking
     }
     return render(request,'home/inicio.html',context)
     
