0a86483a7502d41cb637015c1615ce793c3e1754
Benites Enzo <enzostefani507@gmail.com>
2021-10-19 10:57:43 -0300

    home | fix practica: Majo propuso eliminar los puntos del equipo asignado por profes

--- home/views.py
+++ home/views.py
@@ -9,13 +9,11 @@ def home(request):
     temas = Tema.objects.all()
     unidades = Unidad.objects.all()
     ranking_personas = Usuario.objects.filter(dorado__gt=0).order_by('dorado').reverse()
-    ranking_equipos = Equipo.objects.all().order_by('puntos').reverse()
     context ={
         'nombreSeccion':'Saludos!',
         'contenido': temas,
         'unidades':unidades,
         'ranking_personas': ranking_personas,
-        'ranking_equipos': ranking_equipos
     }
     return render(request,'home/inicio.html',context)
     
