f6eb8ae87c2f4a78c8a05096501528f0da8fb561
Benites Enzo <enzostefani507@gmail.com>
2021-10-28 00:24:26 -0300

    todas las apps | fix: responsive

--- Agilidad/urls.py
+++ Agilidad/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
 from django.conf.urls.static import static
-
+from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('perfil/',include('perfil.urls')),
     path('estado/',include('estado.urls')),
     path('felicidad/',include('felicidad.urls')),
     path('',include('home.urls')),
-]
+]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- static/js/offcanvas.js
+++ static/js/offcanvas.js
@@ -1,6 +1,5 @@
 (function () {
   'use strict'
-
   document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
     document.querySelector('.offcanvas-collapse').classList.toggle('open')
   })
